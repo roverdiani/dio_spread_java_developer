94df259b75bf8a6a56e1f9f778eac6c643950e07
roverdiani <87436745+roverdiani@users.noreply.github.com>
2021-12-29 20:07:40 -0300

    Desafio Bootcamp POO - Concluindo a criação das classes do domínio
    Código referente à aula 6 do desafio "Abstraindo um Bootcamp Usando Orientação a Objetos em Java"

--- desafio-poo/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -2,6 +2,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -47,14 +48,20 @@ public class Dev {
     }
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
     }
 
-    public void calcularTotalXp() {
-
+    public double calcularTotalXp() {
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
     }
 }
